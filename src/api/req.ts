68ac5d6c3926fe9a3e7883e7ab0a0dc14467c7bc
jeremyperwira <jeremyperwira@protonmail.com>
2024-05-24 01:55:37 +0700

    remove skeleton

--- src/api/req.ts
+++ src/api/req.ts
@@ -1,25 +1,19 @@
-// utils/api.ts
 
 export interface Schedule {
-    id: number;
-    station_id: number;
-    stasiun_name: string;
-    arah: string;
-    jadwal: string;
+  id: number;
+  station_id: number;
+  stasiun_name: string;
+  arah: string;
+  jadwal: string;
+}
+
+export const fetchScheduleData = async (apiUrl: string): Promise<Schedule[]> => {
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
-  
-  export const fetchScheduleData = async (apiUrl: string): Promise<Schedule[]> => {
-    const response = await fetch(apiUrl, {
-      headers: {
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-      },
-    });
-  
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-  
-    const result: Schedule[] = await response.json();
-    return result;
-  };
-  
+
+  const result: Schedule[] = await response.json();
+  return result;
+};
