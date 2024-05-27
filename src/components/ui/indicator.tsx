f10a2f0e2b921cd03ea1afa2914a2ab0fed17257
jeremyperwira <jeremyperwira@protonmail.com>
2024-05-27 16:47:13 +0700

    fixing indicator

--- src/components/ui/indicator.tsx
+++ src/components/ui/indicator.tsx
@@ -13,8 +13,8 @@ const OnlineIndicator: React.FC<OnlineIndicatorProps> = ({ isFetching }: OnlineI
     <div className="relative" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <div className={`w-3 h-3 rounded-full ${isFetching ? 'bg-green-500 animate-ping' : 'bg-gray-100'}`}></div>
       {isHovered && (
-          <div className="relative bg-white border border-gray-200 p-2 rounded shadow-md flex flex-row">
-          <p className='text-xs font-normal'>Data Update setiap = <a className='font-bold text-green-500'> 5 Detik </a></p>
+          <div className="dark:bg-zinc-900 dark:border-slate-800 dark:border-2 relative bg-white border border-gray-200 p-2 rounded shadow-md flex flex-row dark:bg-zinc-800">
+          <a className='dark:text-white text-slate-900 text-xs font-normal'>Data Update setiap = <a className='font-bold text-green-500'> 5 Detik </a></a>
         </div>
       )}
     </div>
