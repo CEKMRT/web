7e018357e8b9819a66c0d2ab7d5df1d0560faf76
jeremyperwira <jeremyperwira@protonmail.com>
2024-05-27 14:19:48 +0700

    added more ui, animation

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { fetchScheduleData } from "@/app/api/req";
 import { Skeleton } from "./skeleton";
 import OnlineIndicator from "./indicator";
+
 interface Schedule {
   id: number;
   station_id: number;
@@ -10,75 +11,63 @@ interface Schedule {
   jadwal: string; // (HH:MM)
 }
 
-function JamKeMenit(time: string): number {
+const JamKeMenit = (time: string): number => {
   const [hours, minutes] = time.split(":").map(Number);
   return hours * 60 + minutes;
-}
+};
 
-function FilterData(
+const FilterData = (
   schedules: Schedule[],
   userTime: string | number
-): Schedule[] {
-  // Helper function to convert time from "HH:MM" format to minutes
-  const JamKeMenit = (timeString: string): number => {
-    const [hours, minutes] = timeString.split(":");
-    return parseInt(hours, 10) * 60 + parseInt(minutes, 10);
-  };
-
-  // Convert userTime to minutes
+): Schedule[] => {
   const userTimeInMinutes =
     typeof userTime === "string" ? JamKeMenit(userTime) : userTime;
 
-  // Convert jadwal to minutes, filter schedules, and sort them
   const filteredSchedules = schedules
     .map((schedule) => ({
       ...schedule,
       jadwalInMinutes: JamKeMenit(schedule.jadwal),
     }))
     .filter((schedule) => schedule.jadwalInMinutes >= userTimeInMinutes)
-    .sort((a, b) => a.jadwalInMinutes - b.jadwalInMinutes);
+    .sort((a, b) => a.jadwalInMinutes - b.jadwalInMinutes)
+    .slice(0, 6);
 
-  // Log filtered schedules for debugging
-  console.log("Filtered Schedules:");
-  filteredSchedules.forEach((schedule, index) => {
-    console.log(
-      `${index + 1}. Schedule ID: ${schedule.id}, Jadwal: ${
-        schedule.jadwal
-      }, Jadwal in Minutes: ${schedule.jadwalInMinutes}`
-    );
-  });
+  if (process.env.NODE_ENV === "development") {
+    console.log("Filtered Schedules:");
+    filteredSchedules.forEach((schedule, index) => {
+      console.log(
+        `${index + 1}. Schedule ID: ${schedule.id}, Jadwal: ${
+          schedule.jadwal
+        }, Jadwal in Minutes: ${schedule.jadwalInMinutes}`
+      );
+    });
+  }
 
-  // Return only the first 6 filtered schedules
-  return filteredSchedules.slice(0, 6);
-}
+  return filteredSchedules;
+};
 
-function Jakarta(): string {
+const Jakarta = (): string => {
   const currentTime = new Date();
   const waktuJakarta = new Date(currentTime.getTime() + 7 * 60 * 60 * 1000);
   return waktuJakarta.toISOString().slice(11, 16);
-}
+};
 
-function SelisihWaktu(scheduleTime: string): number | string {
+const SelisihWaktu = (scheduleTime: string): number | string => {
   const currentTime = Jakarta();
-  const currentTimeHours = parseInt(currentTime.slice(0, 2));
-  const currentTimeMinutes = parseInt(currentTime.slice(3, 5));
-
-  const scheduleTimeHours = parseInt(scheduleTime.slice(0, 2));
-  const scheduleTimeMinutes = parseInt(scheduleTime.slice(3, 5));
+  const [currentTimeHours, currentTimeMinutes] = currentTime
+    .split(":")
+    .map(Number);
+  const [scheduleTimeHours, scheduleTimeMinutes] = scheduleTime
+    .split(":")
+    .map(Number);
 
   const diffMinutes =
     (scheduleTimeHours - currentTimeHours) * 60 +
     (scheduleTimeMinutes - currentTimeMinutes);
+  return diffMinutes >= 0 ? diffMinutes : "N/A";
+};
 
-  // Convert to seconds for more accuracy
-  const diffSeconds = diffMinutes * 1;
-
-  return diffSeconds >= 0 ? diffSeconds : "N/A";
-}
-
-function formatTime(time: string): string {
-  return time;
-}
+const formatTime = (time: string): string => time;
 
 const ScheduleComponent: React.FC<{
   apiUrl: string;
@@ -89,8 +78,8 @@ const ScheduleComponent: React.FC<{
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [now, setNow] = useState<string>(Jakarta());
-
   const [isFetching, setIsFetching] = useState(false);
+
   const fetchData = async () => {
     setIsFetching(true);
     try {
@@ -106,60 +95,50 @@ const ScheduleComponent: React.FC<{
 
   useEffect(() => {
     fetchData();
-  
     const dataInterval = setInterval(fetchData, 5000);
     console.log(`Data revalidated at ${Jakarta()} (GMT+7).`);
-  
-    // Update current time every 60 seconds
+
     const timeInterval = setInterval(() => {
       setNow(Jakarta());
     }, 5000);
-  
-    // Cleanup intervals on component unmount
+
     return () => {
       clearInterval(dataInterval);
       clearInterval(timeInterval);
     };
-  }, [apiUrl, setNow]);
-  ; // Run fetchData when apiUrl changes
+  }, [apiUrl]);
 
-  if (loading)
+  if (loading) {
     return (
-      <div className="flex flex-wrap justify-between justify-items-start content-center place-content-evenly gap-4 md:py-4 py-2 max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl px-4 dark:bg-zinc-900 dark:border-slate-800 dark:border-2 z-10ex-col space-y-3 justify-center gap-4 py-4 max-w-sm mx-auto">
-        <Skeleton
-          className=" 
-     w-[450px] h-[20px] rounded-full"
-        />
+      <div className="flex flex-wrap justify-between justify-items-start content-center place-content-evenly gap-4 md:py-4 py-2 max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl px-4 dark:bg-zinc-900 dark:border-slate-800 dark:border-2 z-10">
+        <Skeleton className="w-[450px] h-[20px] rounded-full" />
         <Skeleton className="h-[125px] w-[550px] rounded-xl" />
         <div className="space-y-2">
           <Skeleton className="h-4 w-[600px]" />
-          <h1 className="font-medium text-slate-600/80 "> </h1>
           <Skeleton className="h-4 w-[600px]" />
         </div>
       </div>
     );
-  if (error)
+  }
+
+  if (error) {
     return (
-      <div className="flex flex-wrap justify-between justify-items-start content-center place-content-evenly gap-4 md:py-4 py-2 max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl px-4 dark:bg-zinc-900 dark:border-slate-800 dark:border-2 z-10ex-col space-y-3 justify-center gap-4 py-4 max-w-sm mx-auto">
-        <Skeleton
-          className=" 
- w-[450px] h-[20px] rounded-full"
-        />
+      <div className="flex flex-wrap justify-between justify-items-start content-center place-content-evenly gap-4 md:py-4 py-2 max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl px-4 dark:bg-zinc-900 dark:border-slate-800 dark:border-2 z-10">
+        <Skeleton className="w-[450px] h-[20px] rounded-full" />
         <Skeleton className="h-[125px] w-[550px] rounded-xl bg-red-800/60 dark:bg-red-800" />
         <div className="space-y-2">
-          <Skeleton className="h-4 w-[600px]" />
-          <h1 className="font-medium text-slate-600/80 dark:text-red-800/60 ">
-            {" "}
+          <h1 className="font-medium text-slate-600/80 dark:text-red-800/60">
             Gagal Menampilkan Data -{" "}
             <span className="text-red-800">{error.message}</span>
           </h1>
-          <Skeleton className="h-4 w-[600px]" />
         </div>
       </div>
     );
+  }
 
   const jadwalTerbaru = FilterData(data, now);
   console.log(jadwalTerbaru);
+
   return (
     <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl dark:bg-zinc-950 border-1 dark:border-neutral-800 dark:border-2 z-10">
       <div className="p-6 relative">
@@ -167,17 +146,17 @@ const ScheduleComponent: React.FC<{
           {startStation} &rarr; {endStation}
           <OnlineIndicator isFetching={isFetching} />
         </h2>
-        <p className="text-center text-gray-500 mb-4"></p>
-        <div className="grid grid-cols-3 gap-2 text-center mb-auto pb-10">
+        <div className="grid grid-cols-3 gap-2 text-center mb-auto pt-4 pb-10 ">
           {jadwalTerbaru.length > 0 ? (
-            jadwalTerbaru.slice(0, 6).map((schedule, index) => (
+            jadwalTerbaru.map((schedule, index) => (
               <div
                 key={schedule.id}
-                className={`py-2 rounded-full font-bold ${
+                className={`py-2 rounded-full font-bold 
+                transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ${
                   index === 0 &&
                   (SelisihWaktu(schedule.jadwal) === "N/A" ||
                     parseInt(SelisihWaktu(schedule.jadwal).toString()) < 3)
-                    ? "bg-red-500 text-white"
+                    ? "bg-red-500 text-white hover:animate-ping"
                     : index === 0
                     ? "bg-green-400 text-green-000"
                     : index === 1
@@ -194,39 +173,44 @@ const ScheduleComponent: React.FC<{
             <div className="col-span-3 text-red-500 font-bold">Tutup</div>
           )}
         </div>
-        <div
-          className={`flex justify-around items-center p-1.5 rounded-b absolute inset-x-0 bottom-0 index-10 ${
-            SelisihWaktu(jadwalTerbaru[0].jadwal) === "N/A"
-              ? "bg-red-500 text-white"
-              : parseInt(SelisihWaktu(jadwalTerbaru[0].jadwal).toString()) === 0
-              ? "bg-gray-500 text-white"
-              : parseInt(SelisihWaktu(jadwalTerbaru[0].jadwal).toString()) < 3
-              ? "bg-red-500 text-white"
-              : "bg-green-500 text-white"
-          }`}
-        >
-          <div className="text-sm md:text-sm dark:font-medium">
-            Jadwal Terdekat
-          </div>
-          <div className="text-lg font-bold">{jadwalTerbaru[0].jadwal} </div>
-          <div className="text-sm dark:font-medium">
-            {typeof SelisihWaktu(jadwalTerbaru[0].jadwal) === "number" ? (
-              SelisihWaktu(jadwalTerbaru[0].jadwal) !== 0 ? (
-                <>
-                  Dalam{" "}
-                  <span className="font-bold">
-                    {SelisihWaktu(jadwalTerbaru[0].jadwal)}
-                  </span>{" "}
-                  menit
-                </>
+        {jadwalTerbaru.length > 0 && (
+          <div
+            className={`flex justify-around items-center p-1.5 rounded-b absolute inset-x-0 bottom-0 index-10 ${
+              SelisihWaktu(jadwalTerbaru[0].jadwal) === "N/A"
+                ? "bg-red-500 text-white "
+                : parseInt(SelisihWaktu(jadwalTerbaru[0].jadwal).toString()) ===
+                  0
+                ? "bg-gray-500 text-white"
+                : parseInt(SelisihWaktu(jadwalTerbaru[0].jadwal).toString()) < 3
+                ? "bg-red-500 text-white animate-pulse  "
+                : "bg-green-500 text-white"
+            }`}
+          >
+            <div className="text-sm md:text-sm dark:font-medium">
+              Jadwal Terdekat
+            </div>
+            <div className="text-lg font-bold">{jadwalTerbaru[0].jadwal}</div>
+            <div className="text-sm dark:font-medium">
+              {typeof SelisihWaktu(jadwalTerbaru[0].jadwal) === "number" ? (
+                SelisihWaktu(jadwalTerbaru[0].jadwal) !== 0 ? (
+                  <>
+                    Dalam{" "}
+                    <span className="font-bold">
+                      {SelisihWaktu(jadwalTerbaru[0].jadwal)}
+                    </span>{" "}
+                    menit
+                  </>
+                ) : (
+                  <div className="text-sm font-bold animate-bounce">
+                    Kereta Tiba
+                  </div>
+                )
               ) : (
-                <div className="text-sm font-bold"> Kereta Tiba </div>
-              )
-            ) : (
-              SelisihWaktu(jadwalTerbaru[0].jadwal)
-            )}
+                SelisihWaktu(jadwalTerbaru[0].jadwal)
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- src/components/ui/indicator.tsx
+++ src/components/ui/indicator.tsx
@@ -1,15 +1,24 @@
-// OnlineIndicator.tsx
-
-import React from 'react';
+import React, { useState } from 'react';
 
 interface OnlineIndicatorProps {
   isFetching: boolean;
 }
 
 const OnlineIndicator: React.FC<OnlineIndicatorProps> = ({ isFetching }: OnlineIndicatorProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className={`w-3 h-3 rounded-full ${isFetching ? 'bg-green-500' : 'bg-gray-100'} absolute top-0 left-0`}>
+    <div className='absolute top-0 left-0'>
+
+    <div className="relative" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <div className={`w-3 h-3 rounded-full ${isFetching ? 'bg-green-500 animate-ping' : 'bg-gray-100'}`}></div>
+      {isHovered && (
+          <div className="relative bg-white border border-gray-200 p-2 rounded shadow-md flex flex-row">
+          <p className='text-xs font-normal'>Data Update setiap = <a className='font-bold text-green-500'> 5 Detik </a></p>
+        </div>
+      )}
     </div>
+      </div>
   );
 };
 
