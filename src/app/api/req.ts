6f248db6b140cfa14f20f0204d5d68bee9dec830
jeremyperwira <jeremyperwira@protonmail.com>
2024-05-28 18:22:48 +0700

    *

--- src/app/api/req.ts
+++ src/app/api/req.ts
@@ -1,4 +1,3 @@
-
 export interface Schedule {
   id: number;
   station_id: number;
@@ -6,23 +5,52 @@ export interface Schedule {
   arah: string;
   jadwal: string;
 }
-const authToken = process.env.NEXT_PUBLIC_API_URL;
+// const authToken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtvbWVuZyIsInVzZXJfaWQiOjIsInJvbGUiOiJ1c2VyIiwiZXhwIjoxNzE2OTgwNzQ3fQ.dlDV-3EELKFTrUWD8N1KUmKJQmGVkpdlN5o_DMaIXm8";
+const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwidXNlcl9pZCI6MSwicm9sZSI6ImFkbWluIiwiZXhwIjoxNzQ4NDMxMjYzfQ.o1cRVeLyfWSexHM-atIIbKA7uwTU6jMYaBAWUhWlmtY";
 
-export const fetchScheduleData = async (apiUrl: string): Promise<Schedule[]> => {
+  if (!authToken) {
+    throw new Error('Authorization token is not defined');
+  }
+
+
+export const fetchScheduleData = async (
+  apiUrl: string
+): Promise<Schedule[]> => {
   const response = await fetch(apiUrl, {
     headers: {
       Authorization: `Bearer ${authToken}`,
-      // other headers if needed
+
     },
   });
-
   if (!response.ok) {
-    throw new Error('#404 Server Terputus');
+
+    switch (response.status) {
+      case 400:
+        throw new Error(
+          "Bad Request: The server could not understand the request due to invalid syntax."
+        );
+      case 401:
+        throw new Error(
+          "Unauthorized: Access is denied due to invalid credentials."
+        );
+      case 403:
+        throw new Error(
+          "Forbidden: You do not have permission to access this resource."
+        );
+      case 404:
+        throw new Error(
+          "Not Found: The requested resource could not be found."
+        );
+      case 500:
+        throw new Error(
+          "Internal Server Error: The server encountered an internal error and was unable to complete your request."
+        );
+      default:
+        throw new Error(`Unexpected error: ${response.statusText}`);
+    }
   }
-  if (!apiUrl) {
-    throw new Error('API URL is not defined');
-  }
+
 
   const result: Schedule[] = await response.json();
   return result;
-}
+};
