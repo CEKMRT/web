9b57d4f6a7cc9c8888ddbce719c6155dfa2a8666
jeremyperwira <jeremyperwira@protonmail.com>
2024-05-28 18:55:37 +0700

    *

--- src/app/api/req.ts
+++ src/app/api/req.ts
@@ -6,52 +6,46 @@ export interface Schedule {
   jadwal: string;
 }
 
-
-  
-  
-  export const fetchScheduleData = async (
-    apiUrl: string
-  ): Promise<Schedule[]> => {
+export const fetchScheduleData = async (
+  apiUrl: string
+): Promise<Schedule[]> => {
   const authToken = process.env.NEXT_PUBLIC_AUTH_TOKEN;
 
   if (!authToken) {
-    throw new Error('Authorization token is not defined');
+    throw new Error("Authorization token is not defined");
   }
-  
+
   const response = await fetch(apiUrl, {
     headers: {
       Authorization: `Bearer ${authToken}`,
-
     },
   });
   if (!response.ok) {
-
     switch (response.status) {
       case 400:
         throw new Error(
-          "Bad Request: The server could not understand the request due to invalid syntax."
+          "Permintaan Buruk: Server tidak dapat memahami permintaan karena sintaks yang tidak valid."
         );
       case 401:
         throw new Error(
-          "Unauthorized: Access is denied due to invalid credentials."
+          "Tidak Sah: Akses ditolak karena kredensial yang tidak valid."
         );
       case 403:
         throw new Error(
-          "Forbidden: You do not have permission to access this resource."
+          "Dilarang: Anda tidak memiliki izin untuk mengakses sumber daya ini."
         );
       case 404:
         throw new Error(
-          "Not Found: The requested resource could not be found."
+          "Tidak Ditemukan: Sumber daya yang diminta tidak dapat ditemukan."
         );
       case 500:
         throw new Error(
-          "Internal Server Error: The server encountered an internal error and was unable to complete your request."
+          "Kesalahan Server Internal: Server mengalami kesalahan internal dan tidak dapat menyelesaikan permintaan Anda."
         );
       default:
-        throw new Error(`Unexpected error: ${response.statusText}`);
+        throw new Error(`Kesalahan tak terduga: ${response.statusText}`);
     }
   }
-
 
   const result: Schedule[] = await response.json();
   return result;
