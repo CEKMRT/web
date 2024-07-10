d649862bd926cee5355b762a580519810e44568b
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-11 06:52:13 +0700

    set default domain

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -12,9 +12,12 @@ async function getMRTNews(): Promise<MRTNewsItem[]> {
     }
   }
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/news`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL || "https://www.cekmrt.xyz"}/api/news`,
+    {
+      cache: "no-store",
+    }
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch news");
   }
