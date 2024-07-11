1755ff179f9f5fce35173b5107d6c92db3e8c871
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-11 18:13:09 +0700

    global remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
-// Import the necessary modules
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     CUSTOM_API_URL: process.env.CUSTOM_API_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   // Other Next.js configuration options
 };
